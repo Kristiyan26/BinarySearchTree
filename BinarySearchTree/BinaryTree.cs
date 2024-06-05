f7e470d3db02deeb9acd51469ef86de96b6e1e6c
Kristiyan Lyubenov <KristiyanLyubenov26@gmail.com>
2024-06-06 00:48:26 +0300

    Add project files.

---
+++ BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinaryTree
+    {
+        public TreeNode Root { get; set; }
+
+        public BinaryTree()
+        {
+            Root = null;
+        }
+
+
+        public int NumberOfLeaves(TreeNode node)
+        {
+            if(node == null)
+            {
+                return 0;
+            }
+            else if(node.Left==null && node.Right == null)
+            {
+                return 1;
+            }
+
+            return NumberOfLeaves(node.Left) + NumberOfLeaves(node.Right);  
+        }
+    }
+}
---
+++ BinarySearchTree/TreeNode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class TreeNode
+    {
+        private TreeNode left;
+        private TreeNode right;
+
+        public int Data { get; set; }
+        public TreeNode Left {
+            get
+            {
+                return this.left;
+            }
+            set
+            {
+                this.left = value;
+            }
+        }
+        public TreeNode Right
+        {
+            get
+            {
+                return this.right;
+            }
+            set
+            { 
+                this.right = value;
+            }
+        }
+        public TreeNode(int data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+    }
+}
